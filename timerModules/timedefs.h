fb84e5641d4ef6e9bee0a47f7542ba49dd5c5c41
umarfaruq <umar.farooq@xflowresearch.com>
2015-05-18 11:48:40 +0500

    timer Defines v2
    Added definitions and sleep functions.

--- timerModules/timedefs.h
+++ timerModules/timedefs.h
@@ -1,7 +1,7 @@
 #ifndef _TIMEDEFS_H_
 #define _TIMEDEFS_H_
 
-
+#define MILLION 1000000UL
 #define BURST_TX_DRAIN_US 200
 struct timestamp_option
 {
@@ -11,16 +11,42 @@ struct timestamp_option
 	uint32_t echo_timestamp;
 } __attribute__ ((__packed__));
 
+const static uint64_t one_sec		= rte_get_tsc_hz();
+const static uint64_t one_msec		= (one_sec/1000UL);
+const static uint64_t one_usec 		= (one_msec/1000UL);
+const static uint64_t one_nsec 		= (one_usec/1000UL);
 
-uint64_t prev_tsc = 0, prev_arp = 0, prev_disp = 0, cur_tsc = 0, timer_tsc = 0;
+uint64_t prev_tsc = 0, cur_tsc = 0, timer_tsc = 0;
 
-static uint64_t one_msec		= 0;
-static uint64_t one_sec			= 0;
-static uint64_t one_usec 		= 0;
-static uint64_t one_nsec 		= 0;
+#define diff_tsc abs(cur_tsc - prev_tsc)	//--!check for bugs
 
-uint64_t diff_tsc abs(cur_tsc - prev_tsc)
-
+/**
+This function provides a sleep in msec
+*/
+static inline void sleep_msec(unsigned msec) {	//--! should it be in the timer_functions file?
+	uint64_t curr_sleep = 0, prev_sleep = rte_rdtsc()/one_msec;
+	while(curr_sleep - prev_sleep <= msec) 
+		curr_sleep = rte_rdtsc()/one_msec;
+	return;
+}
+/**
+This function provides a sleep in usec
+*/
+static inline void sleep_usec(unsigned sec) {	//--! should it be in the timer_functions file?
+	uint64_t curr_sleep = 0, prev_sleep = rte_rdtsc()/one_usec;
+	while(curr_sleep - prev_sleep <= usec)
+		curr_sleep = rte_rdtsc()/one_usec;
+	return;
+}
+/**
+This function provides a sleep in nsec
+*/
+static inline void sleep_nsec(unsigned nsec) {	//--! should it be in the timer_functions file?
+	uint64_t curr_sleep = 0, prev_sleep = rte_rdtsc()/one_nsec;
+	while(curr_sleep - prev_sleep <= nsec)
+		curr_sleep = rte_rdtsc()/one_nsec;
+	return;
+}
 
 
 #endif
