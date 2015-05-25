5f4d332c4ab3ac7cd2bf129829a70d543c9d9f37
Umar Farooq <umar1.farooq1@gmail.com>
2015-05-25 18:09:40 +0500

    lcore management structure define and additions in port management module

---
+++ Twister/coremgmt/lcoreconf.h
@@ -0,0 +1,20 @@
+#ifndef _LCORECONF_H_
+#define _LCORECONF_H_
+
+#include <rte_per_lcore.h>
+#include <portconf.h>
+
+
+#define MAX_LCORES 16
+#define MAX_PORTS_PER_LCORE 8
+#define MAX_TX_PKT_BURST 32
+
+struct lcore_conf {
+	uint8_t socket_id;
+	uint8_t num_ports;
+	uint8_t managed_port[MAX_ETH_PORTS];
+};
+
+struct lcore_conf lcore_conf[MAX_LCORES];
+
+#endif
