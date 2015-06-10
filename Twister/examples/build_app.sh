e95f9094ce315349c02cf5d4627757a4b85933f1
root <chaudhryusama@gmail.com>
2015-06-10 11:40:30 +0000

    Updated the Example

--- Twister/examples/build_app.sh
+++ Twister/examples/build_app.sh
@@ -3,5 +3,5 @@ make clean
 make
 cd examples
 rm -rf app
-gcc test.c ../build/libtwister.a ../dpdk_libs/libdpdk.a -I../include -I../../dpdk/x86_64-native-linuxapp-gcc/include -lpthread -lrt -ldl -mssse3 -lm -o app
+gcc test.c ../build/libtwister.a ../../dpdk/libdpdk.a -I../include -I../../dpdk/x86_64-native-linuxapp-gcc/include -lpthread -lrt -ldl -mssse3 -lm -o app
 ./app -c 1 -n 1
