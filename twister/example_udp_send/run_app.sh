aedd8e0f497870e1f4f02f78cacab8c507a70064
haris00 <haristanvir@hotmail.com>
2015-07-30 12:49:40 +0500

    Update run_app.sh

--- twister/example_udp_send/run_app.sh
+++ twister/example_udp_send/run_app.sh
@@ -5,14 +5,13 @@ then
 else
     echo "Process not running, starting..."
     screen -dm ./udp_send_app -c 1 -n 4 -b 00:03.0 -- -p 1
-    sleep 1
     while :
     do
+        sleep 1
         if pidof -x "udp_send_app">/dev/null;
         then
          break
         else
-         sleep 1
          screen -dm ./udp_send_app -c 1 -n 4 -b 00:03.0 -- -p 1
         fi
     done
