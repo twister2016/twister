10636e17babebb41a4f3603780c677c2cf84e2d5
Chaudhry Muhammad Usama <chaudhryusama@gmail.com>
2015-09-08 20:59:32 +0500

    Merge origin/devlibuv into devlibuv
    Conflicts:
    	twister/example_udp_echo/twister.mk

--- twister/example_udp_echo/run.sh
+++ twister/example_udp_echo/run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sudo ./UDP_ECHO.exe -c 1 -n 4 -b 00:03.0 -- -p 1
+sudo modprobe uio
+insmod $RTE_SDK/$RTE_TARGET/kmod/igb_uio.ko
+sudo ifconfig eth1 0 down
+sudo $RTE_SDK/tools/dpdk_nic_bind.py --bind=igb_uio eth1
+sudo ./UDP_ECHO.exe
