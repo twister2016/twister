dd5dcbe834f2d44d1923a3114907572bff13510d
Chaudhry Muhammad Usama <chaudhryusama@gmail.com>
2015-09-15 12:07:36 -0700

    Clean the examples of the twister and make the Makefile more generic

--- twister/install.sh
+++ twister/install.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 make clean
 make
-sudo mkdir -p /usr/lib/twister
-sudo cp $(RTE_SDK)/x86_64-native-linuxapp-gcc/lib/* /usr/lib/twister/.
-sudo cp build/libtwister.a /usr/lib/twister/.
-
+sudo mkdir -p /home/twister
+sudo chmod 777 /home/twister
+cp ./build/libtwister.a /home/twister/.
+cp /usr/dpdk/x86_64-native-linuxapp-gcc/lib/* /home/twister/
 echo 'install all twister headers'
-sudo cp  include/*.h /usr/include/.
-sudo cp $(RTE_SDK)/x86_64-native-linuxapp-gcc/include/*.h /usr/include/.
-sudo mkdir -p /usr/include/generic/.
-sudo mkdir -p /usr/include/exec-env/.
-sudo cp $(RTE_SDK)/x86_64-native-linuxapp-gcc/include/generic/*.h /usr/include/generic/.
-sudo cp $(RTE_SDK)/x86_64-native-linuxapp-gcc/include/exec-env/*.h /usr/include/exec-env/.
-
+mkdir -p /home/twister/include/.
+cp  ./include/*.h /home/twister/include
+cp /usr/dpdk/x86_64-native-linuxapp-gcc/include/*.h /home/twister/
+mkdir -p /home/twister/generic/.
+mkdir -p /home/twister/exec-env/.
+cp /usr/dpdk/x86_64-native-linuxapp-gcc/include/generic/*.h /home/twister/generic/
+cp /usr/dpdk/x86_64-native-linuxapp-gcc/include/exec-env/*.h /home/twister/exec-env/
 echo "transfering twister.mk"
-sudo cp twister.mk /usr/include
- 
+cp ./mk/twister.mk /home/twister/
 echo "DONE........."
