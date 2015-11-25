7a9000abdfbe20e6fd945a0b42dfd61804bb0291
Chaudhry Muhammad Usama <chaudhryusama@gmail.com>
2015-11-25 15:47:56 +0500

    Modified the Ping Application

--- examples/example_icmp_echo/test.c
+++ examples/example_icmp_echo/test.c
@@ -9,10 +9,13 @@
 #include <inttypes.h>
 #include <math.h>
 #include <time.h>
+#include <stdbool.h>
+#include <arpa/inet.h>
+#include<signal.h>
+#include<unistd.h>
 
 
 #define ICMP_PROTO_ID	1
-#define DES_ADDR 181169931
 #define BUFF_SIZE 74
 
 struct icmp_echo{
@@ -26,6 +29,7 @@ struct icmp_echo{
 
 unsigned short calcsum(unsigned short *buffer, int length);
 int main(int, char **);
+static int console_input(int*);
 int user_app_main(void *);
 void pkt_rx(tw_rx_t *, tw_buf_t *);
 void pkt_tx(tw_tx_t *);
@@ -33,24 +37,41 @@ struct ether_hdr * eth;
 struct ipv4_hdr * ip;
 struct icmp_echo* icmp;
 int phy_port_id;
-int finishing = 0;
+int ping=-1;
+uint32_t ping_ip,ping_count;
 double time_ms, t1 ,t2;
 uint16_t eth_type;
 static struct ether_addr * dst_eth_addr;
+
+
+
+void sig_handler(int signo)
+{
+  if (signo == SIGINT){
+     ping=-1;
+     printf("\n>>");
+
+  }
+}
+bool isValidIpAddress(char *ipAddress);
+///////////////////////
+
+
+bool isValidIpAddress(char *ipAddress)
+{
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, ipAddress, &(sa.sin_addr));
+    return result != 0;
+}
 
 double  current_timestamp() {
     clock_t start = clock();
     double elapsed = (double)(start) * 1000.0 / CLOCKS_PER_SEC;
      return elapsed;
 }
-void sleepWrapper (unsigned int secs) {
-    unsigned int left = secs;
-    while ((left > 0) && (!finishing)) // Don't continue if signal has
-        left = sleep (left);           //   indicated exit needed.
-}
+
 unsigned short calcsum(unsigned short *buffer, int length){
 	unsigned long sum;
-
 	// initialize sum to zero and loop until length (in words) is 0
 	for (sum=0; length>1; length-=2) // sizeof() returns number of bytes, we're interested in number of words
 		sum += *buffer++;	// add 1 word of buffer to sum and proceed to the next
@@ -64,32 +85,65 @@ unsigned short calcsum(unsigned short *buffer, int length){
 	return ~sum;
 }
 void pkt_rx(tw_rx_t * handle, tw_buf_t * buffer) {
-   eth = buffer->data;
+    eth = buffer->data;
     eth_type = tw_be_to_cpu_16(eth->ether_type);
-           switch (eth_type) {
-            case ETHER_TYPE_ARP:
-                tw_arp_parser(buffer, "tw0");
-                break;
-            case ETHER_TYPE_IPv4:
-                icmp = buffer->data+sizeof(struct ether_hdr*);
-                time_ms= icmp->time;
-                //printf("time 1 milliseconds: %lld\n", time_ms);
-                double t1=current_timestamp();
-                time_ms=t1-t2;
-                //printf("milliseconds current: %lld\n", t);
-                printf("milliseconds difference: %f\n", time_ms);
-                sleepWrapper(1);
-                break;
-           }
+    switch (eth_type) {
+        case ETHER_TYPE_ARP:
+            tw_arp_parser(buffer, "tw0");
+            break;
+        case ETHER_TYPE_IPv4:
+            ip = (struct ipv4_hdr*) (eth + 1);
+            switch (ip->next_proto_id) {
+                case ICMP_PROTO_ID:
+                    icmp = (struct icmp_echo*) (ip + 1);
+                    time_ms = icmp->time;
+                    double t1 = current_timestamp();
+                    time_ms = t1 - t2;
+                    struct in_addr ip_addr;
+                    ip_addr.s_addr = ip->src_addr;
+                    printf("64 bytes from %s: icmp_seq=%d ttl=%d time=%fms\n", inet_ntoa(ip_addr),icmp->sequence, ip->time_to_live,time_ms);
+                    break;
+            }
+    }
     tw_free_pkt(buffer);
     return;
 }
-void pkt_tx(tw_tx_t * handle) {
 
+static int console_input(int* dont){
+    char line[1024];
+    printf("\e[1;1H\e[2J");
+    printf("\n ******** TWISTER PING APPLICATION ***********\n");
+    printf("\n enter IP address i.e., ping <xx.xx.xx.xx> or <exit>");
+    printf("\n>>");
+
+    while(1){
+        fgets (line, 1024, stdin);
+        line[strlen(line)-1] = 0;
+        if (isValidIpAddress(line)){
+            ping_ip=tw_convert_ip_str_to_dec(line);
+            ping=1;
+        }
+        else if (strcmp(line, "exit") == 0) {
+            printf ("exiting ......\n");
+            exit(0);
+        }
+        
+        else{
+            printf("command not valid\n");
+            printf("\n>>");
+        }
+
+       
+    }    
+    return 0;
+}
+
+void pkt_tx(tw_tx_t * handle) {
+    if(ping==1){
     if (unlikely(dst_eth_addr) == NULL) {
-        struct arp_table * temp_arp_entry = tw_search_arp_table(tw_be_to_cpu_32(DES_ADDR));
+        struct arp_table * temp_arp_entry = tw_search_arp_table(tw_be_to_cpu_32(ping_ip));
         if(temp_arp_entry == NULL) {
-            tw_construct_arp_packet(DES_ADDR, phy_port_id);
+            tw_construct_arp_packet(ping_ip, phy_port_id);
         }
         else
             dst_eth_addr = &temp_arp_entry->eth_mac;
@@ -102,8 +156,8 @@ void pkt_tx(tw_tx_t * handle) {
     icmp->type		= 8;		/* icmp echo */
     icmp->code		= 0;		/* only valid value for echo or echo reply */
     icmp->identifier	= 0x1337;	/* the id we'll be using to distinguish our data from other icmp packets */
-									// ^^^ Actually it won't. It is now distinguished with the ip header
-    icmp->sequence		= 0;		/* our initial sequence will be zero
+    ping_count++;									// ^^^ Actually it won't. It is now distinguished with the ip header
+    icmp->sequence		= ping_count;		/* our initial sequence will be zero
 	/* just like IP we have to zero the checksum before we calculate it */
     icmp->time=0;
     icmp->time=current_timestamp();
@@ -116,7 +170,7 @@ void pkt_tx(tw_tx_t * handle) {
     ip->next_proto_id = ICMP_PROTO_ID;
     ip->src_addr = tw_cpu_to_be_32(tw_get_ip_addr("tw0"));
 
-    ip->dst_addr = tw_cpu_to_be_32(DES_ADDR);
+    ip->dst_addr = tw_cpu_to_be_32(ping_ip);
     ip->version_ihl = 0x45;
     ip->time_to_live = 63;
     ip->hdr_checksum = 0;
@@ -125,14 +179,18 @@ void pkt_tx(tw_tx_t * handle) {
     tw_copy_ether_addr(dst_eth_addr, &(eth->d_addr));
     tw_copy_ether_addr(tw_get_ether_addr("tw0"), &(eth->s_addr));
     tw_send_pkt(tx_buf, "tw0");
-    sleepWrapper(1);
     tw_free(tx_buf);
+    }
     }
 }
 int main(int argc, char **argv) {
     tw_init_global(argc, argv);
+    Printing_Enable = 0; //disable the real-time printing of Tx/Rx,
     tw_map_port_to_engine("tw0", "engine0");
     phy_port_id = tw_eth_name_to_id("tw0");
+    if (signal(SIGINT, sig_handler) == SIG_ERR)
+        printf("\ncan't catch SIGINT\n");
+    tw_launch_engine(console_input,NULL, "engine1");
     user_app_main(NULL);
     return 0;
 }
