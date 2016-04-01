475b2344993e952f5c62ac905f17f0f8706b106b
Chaudhry Usama <chaudhryusama@gmail.com>
2016-04-01 05:58:39 -0700

    Same as previous
    Signed-off-by: Chaudhry Usama <chaudhryusama@gmail.com>

--- twister/install.sh
+++ twister/install.sh
@@ -4,15 +4,15 @@ make
 sudo mkdir -p /home/twister
 sudo chmod 777 -R /home/twister
 cp ./build/libtwister.a /home/twister/.
-cp $RTE_SDK/$RTE_TARGET/lib/* /home/twister/
+cp ../dpdk/x86_64-native-linuxapp-gcc/lib/* /home/twister/
 echo 'install all twister headers'
 mkdir -p /home/twister/include/.
 cp  ./include/*.h /home/twister/include
-cp $RTE_SDK/$RTE_TARGET/include/*.h /home/twister/
+cp ../dpdk/x86_64-native-linuxapp-gcc/include/*.h /home/twister/
 mkdir -p /home/twister/generic/.
 mkdir -p /home/twister/exec-env/.
-cp $RTE_SDK/$RTE_TARGET/include/generic/*.h /home/twister/generic/
-cp $RTE_SDK/$RTE_TARGET/include/exec-env/*.h /home/twister/exec-env/
+cp ../dpdk/x86_64-native-linuxapp-gcc/include/generic/*.h /home/twister/generic/
+cp ../dpdk/x86_64-native-linuxapp-gcc/include/exec-env/*.h /home/twister/exec-env/
 echo "transfering twister.mk"
 sudo cp ./mk/twister.mk /usr/include
 echo "DONE........."
