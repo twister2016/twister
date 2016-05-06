728037c384f22e1e1fb3dcfd9a928e992f481683
Waqas Riaz <waqasriaz234@gmail.com>
2016-05-06 12:32:06 +0000

    Resolve #56
    Signed-off-by: Waqas Riaz <waqasriaz234@gmail.com>

--- twister/mk/tw_config.py
+++ twister/mk/tw_config.py
@@ -3,7 +3,9 @@
 import subprocess
 import json
 import ConfigParser
+import dpdk_nic_bind as lib_dpdk
 
+dpdk_drivers =["igb_uio", "vfio-pci", "uio_pci_generic"]
 
 def populate_lshw_paramas():
     ports = []
@@ -23,6 +25,24 @@ def populate_lshw_paramas():
                         ports.append(tempdic)
 
     return ports
+
+
+
+
+def get_kernel_drv(devices):
+
+    global dpdk_drivers
+    kernel_drv = []
+
+    for d in devices.keys():
+        if not lib_dpdk.has_driver(d):
+            continue
+        if devices[d]["Driver_str"] not in dpdk_drivers:
+            kernel_drv.append(devices[d])
+            return kernel_drv[0]['Driver_str']
+    return None
+
+
 
 def get_pci_address(sys_tree, interface_list):
     pci_list = []
@@ -44,33 +64,69 @@ def check_bind_devices(cmd):
             bind_dpdk_devices.append(line.split()[0])
     return bind_dpdk_devices
 
-def unbind_device(cmd, device):
+def bind_device(cmd, device, driver):
 
-    status = subprocess.Popen([cmd, '-b', 'ixgbevf',  device], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    status = subprocess.Popen(['sudo', cmd, '-b', driver,  device], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return status.communicate()
+
+#def unbind_device(cmd, device, driver):
+#
+#    status = subprocess.Popen(['sudo', cmd, '-b', driver,  device], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#    return status.communicate()
 
 def get_total_cpus():
 
     cpu_status = subprocess.Popen(["/usr/bin/lscpu"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = cpu_status.communicate()
-
     total_cpus = int(out.split()[11]) -1
     return total_cpus
 
+def get_whitelist(devices, config):
+    whitelist = config.get('DEFAULT', 'whitelist').split(',')
+    whitelist_pci = []
+    for device_name in whitelist:
+        whitelist_pci.append(lib_dpdk.dev_id_from_dev_name(device_name))
+    return whitelist_pci
+
+
+
+def get_blacklist(devices, config):
+    blacklist = config.get('DEFAULT', 'blacklist').split(',')
+    blacklist_pci = []
+    for device_name in blacklist:
+        blacklist_pci.append(lib_dpdk.dev_id_from_dev_name(device_name))
+    return blacklist_pci
+
+
+def get_coremask(config):
+    cores = int(config.get('DEFAULT', 'cores'))
+    total_cpus = get_total_cpus()
+    coremask = hex((2**total_cpus-1) << cores)
+    return coremask
+
 def parse_twister_params(twister_conf):
+    pass
 
     config = ConfigParser.RawConfigParser()   
     config.readfp(open(twister_conf))
 
     blacklist = config.get('DEFAULT', 'blacklist').split(',')
+    blacklist_pci = get_pci_address(sys_tree, blacklist)
+
+
+
     whitelist = config.get('DEFAULT', 'whitelist').split(',')
+    whitelist_pci = get_pci_address(sys_tree, whitelist)
+
+
+
     cores = int(config.get('DEFAULT', 'cores'))
+    coremask = hex((2**total_cpus-1) << cores)
 
     print "blacklist " , blacklist
     print "whitelist" , whitelist
 
     sys_tree = populate_lshw_paramas()
-    blacklist_pci = get_pci_address(sys_tree, blacklist)
 
     if not set(whitelist).issubset([dev['logicalname'] for dev in sys_tree ]):
         raise Exception("whitelist interfaces in twister.conf do not exist")
@@ -78,30 +134,62 @@ def parse_twister_params(twister_conf):
     if not set(whitelist).isdisjoint(blacklist):
         raise Exception("whitelist conflicts with blacklist in twister.conf")
     total_cpus = get_total_cpus()
-    coremask = hex((2**total_cpus-1) << cores)
     portmask = hex(len(whitelist))
 
 
-    return coremask, portmask, blacklist_pci
+    return coremask, portmask, blacklist_pci, whitelist_pci
+
+def bind_all_to_linux(cmd, devices):
+    
+    kernel_driver = get_kernel_drv(devices)
+    for dev in devices.keys():
+        bind_device(cmd, dev, kernel_driver)
 
 
 
-if __name__ == '__main__':
-
+def main():
     cmd = "/home/twister/config/dpdk_nic_bind.py"
     twister_conf = "/home/twister/config/twister.conf"
     twister_api = "/home/twister/config/twister_api.json"
+    dpdk_drv = "igb_uio"
 
-    bind_dpdk_dev = check_bind_devices(cmd)
-    print "These devices are already binded \n" , bind_dpdk_dev
 
-    for device in bind_dpdk_dev:
-        out, err = unbind_device(cmd, device)
+    lib_dpdk.check_modules()
+    lib_dpdk.get_nic_details()
+    devices = lib_dpdk.devices
 
-    coremask, portmask, blacklist = parse_twister_params(twister_conf)
+
+    bind_all_to_linux(cmd, devices)
+
+    config = ConfigParser.RawConfigParser()
+    config.readfp(open(twister_conf))
+
+
+    whitelist = get_whitelist(devices, config)
+    blacklist = get_blacklist(devices, config)
+    coremask = get_coremask(config)
+
+
+    if not set(whitelist).isdisjoint(blacklist):
+        raise Exception("whitelist conflicts with blacklist in twister.conf")
+
+
+    if not set(whitelist).issubset(devices.keys()):
+        raise Exception("whitelist interfaces in twister.conf do not exist")
+
+
+    portmask = hex(len(whitelist))
+
+    for device in whitelist:
+        out, err = bind_device(cmd, device, dpdk_drv)
 
     twister_json = { "coremask": coremask, "portmask": portmask, "blacklist": blacklist}
     with open(twister_api, 'w') as outfile:
         json.dump(twister_json, outfile)
 
     print twister_json
+
+
+if __name__ == '__main__':
+
+    main()
