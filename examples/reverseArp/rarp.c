1aea87eca81d8d8225453aa64b64c5b283bd595e
Usama <10beeumehboob@seecs.edu.pk>
2016-05-11 22:54:26 +0100

    reverse arp application added to bin folder and removed console_input

--- examples/reverseArp/rarp.c
+++ examples/reverseArp/rarp.c
@@ -20,7 +20,6 @@ struct ether_addr requestmac = {
 #define BUFF_SIZE 42 //sizeof eth hdr = 14 + sizeof arp hdr = 28
 
 int rarp_flag = -1;
-int rarp_resolved_reply = 0;
 
 //////prototypes//////
 int main(int, char **);
@@ -37,47 +36,6 @@ bool isValidIpAddress(char *ipAddress) {
     return result != 0;
 }
 
-static int console_input(int* dont) {
-    char line[1024];
-    printf("\e[1;1H\e[2J");
-    printf("\n ******** TWISTER RARP APPLICATION ***********\n");
-    printf("\n enter <send rarp> or <exit>");
-    printf("\n>>");
-    while (1) {
-
-        if(fgets(line, 1024, stdin) != NULL)
-	{
-        line[strlen(line) - 1] = 0;
-        if (strcmp(line, "send rarp") == 0) {
-            rarp_flag = 1;
-            rarp_resolved_reply = 0;
-            uint8_t ip_heartbeat = 0;
-            while (rarp_resolved_reply != 1) { 
-                /*if we don't received reply in 4 seconds,
-                  let the user know that reverse arp is not resolved.*/ 
-                ip_heartbeat++;
-                usleep(1000000);
-                if (ip_heartbeat > 4) {
-                    printf("RARP not resolved\n");
-                    break;
-                }
-            }
-            printf("\n>>");
-
-        } else if (strcmp(line, "exit") == 0) {
-            printf("exiting ......\n");
-            exit(0);
-        } else {
-            printf("command not valid\n");
-            printf("\n>>");
-        }
-
-	}
-
-    }
-    return 0;
-}
-
 void reply_payload(tw_rx_t * handle, tw_buf_t * buffer) {
     struct ether_addr * dst_eth_addr;
     struct ether_hdr * eth = buffer->data;
@@ -91,7 +49,7 @@ void reply_payload(tw_rx_t * handle, tw_buf_t * buffer) {
             sender_ip = arp_pkt->arp_data.arp_sip;
             struct in_addr ip_addr;
             ip_addr.s_addr = (target_ip);
-            if (rarp_resolved_reply == 0) { /* if user is waiting for reply and we have
+            if (rarp_flag == 1) { /* if user is waiting for reply and we have
                                              * received the reply, print it
                                              */
                 printf("Resolved IP for MAC(%02X:%02X:%02X:%02X:%02X:%02X) = %s \n ",
@@ -102,18 +60,18 @@ void reply_payload(tw_rx_t * handle, tw_buf_t * buffer) {
                     eth->d_addr.addr_bytes[4],
                     eth->d_addr.addr_bytes[5],  inet_ntoa(ip_addr));
             
-                rarp_resolved_reply = 1;                        
+                exit(1);
             }
-            rarp_flag = -1;
-            break;
     }
-
     tw_free_pkt(buffer);
 
 }
 
 void check_arp(int* no) {
+    static rarp_count = 0;
     if (rarp_flag == 1) {
+        
+        rarp_count++;    
 
         int port_id = tw_eth_name_to_id("tw0");
         tw_buf_t * tx_buf = tw_new_buffer(BUFF_SIZE);
@@ -128,22 +86,23 @@ void check_arp(int* no) {
         ether_addr_copy(tw_get_ether_addr("tw0"), &(rarp->arp_data.arp_tha));
         rarp->arp_data.arp_sip = 0;
         rarp->arp_data.arp_tip = 0;
-
         eth->ether_type = tw_cpu_to_be_16(ETHER_TYPE_RARP);
         tw_copy_ether_addr(&(broadcastmac), &(eth->d_addr));
         tw_copy_ether_addr(tw_get_ether_addr("tw0"), &(eth->s_addr));
         tw_send_pkt(tx_buf, "tw0");
         tw_free(tx_buf);
-
-        rarp_flag = -1;
+    }
+    if (rarp_count > 4 && rarp_flag ==1 ){
+        printf("Address not resolved.\n");
+        exit(0);
     }
 }
 
 int main(int argc, char **argv) {
-    tw_init_global(argc, argv);
+    tw_init_global();
     Printing_Enable = 0; //disable the real-time printing of Tx/Rx,
     tw_map_port_to_engine("tw0", "engine0");
-    tw_launch_engine(console_input, NULL, "engine1");
+    rarp_flag =1;
     user_app_main(NULL);
 }
 
