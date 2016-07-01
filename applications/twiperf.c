8092068c8ef72463bd157a6a67356efa71f371e8
Aamir Khan <aamir.khan@xflowresearch.com>
2016-07-01 15:21:34 +0500

    Resolved a minor bug in offset calculation in twiperf.c

--- applications/twiperf.c
+++ applications/twiperf.c
@@ -35,7 +35,7 @@ uint64_t current;
 int64_t jitter1;
 struct iperf_test test;
 struct iperf_stats test_stats;
-int offset, offset1;
+int offset;
 void print_perf_stats(tw_timer_t * timer_handle);
 int udp_app_server(void *);
 void reply_udp_payload(tw_rx_t *, tw_buf_t *);
@@ -370,7 +370,7 @@ void pkt_tx(tw_tx_t * handle)
     if((curr_time_cycle - prev_time_cycle) > pps_delay)
     {
         prev_time_cycle = curr_time_cycle;
-        test.app = test.tx_buf->data + offset1;
+        test.app = test.tx_buf->data + offset;
         test.app->payload = tw_get_current_timer_cycles();
 
         tw_send_pkt(test.tx_buf, "tw0");
@@ -491,7 +491,6 @@ int main(int argc, char **argv)
 {
     tw_init_global();
     offset =  sizeof(struct ether_hdr) + sizeof(struct ipv4_hdr) + sizeof(struct udp_hdr);
-    offset1 = sizeof(struct ether_hdr) + sizeof(struct ipv4_hdr) + sizeof(struct app_hdr);
     if(twiperf_parse_arguments(&test, argc, argv) < 0)
     {
         iperf_err(&test, "parameter error - %s", iperf_strerror(i_errno));
